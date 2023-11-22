affa730aff0ddb0cae06542d6ea5e90706e944a8
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-11-22 13:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+// script.js
+function swapTheme() {
+    const appDiv = document.getElementById('app');
+    const swapButton = document.getElementById('swap');
+
+    // Toggle between day and night classes
+    appDiv.classList.toggle('day');
+    appDiv.classList.toggle('night');
+
+    // Toggle between button_day and button_night classes
+    swapButton.classList.toggle('button_day');
+    swapButton.classList.toggle('button_night');
+}
+
 
